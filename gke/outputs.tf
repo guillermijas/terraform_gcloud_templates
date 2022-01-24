583d01c1b04755c3cd9c3b3f59988a348a320ede
Guillermo Mora <guillermo@cleverppc.com>
2022-01-24 10:45:16 +0100

    Update Terraform version GKE

--- gke/outputs.tf
+++ gke/outputs.tf
@@ -1,21 +1,11 @@
-output "region" {
-  value = var.region
-  description = "GCloud Region"
+output "kubernetes_cluster_name" {
+  value = google_container_cluster.primary.name
+  description = "GKE Cluster Name"
 }
 
 output "zone" {
   value = var.zone
   description = "GCloud Zone"
-}
-
-output "gcloud_project" {
-  value = var.gcloud_project
-  description = "GCloud Project ID"
-}
-
-output "kubernetes_cluster_name" {
-  value = google_container_cluster.primary.name
-  description = "GKE Cluster Name"
 }
 
 output "kubernetes_cluster_host" {
--- gke/variables.tf
+++ gke/variables.tf
@@ -3,22 +3,5 @@ variable "gcloud_project" { description = "project id in Google Cloud" }
 variable "region" { default = "europe-west1" }
 variable "zone" { default = "europe-west1-b" }
 
-variable "gke_username" {
-  default     = ""
-  description = "gke username"
-}
-
-variable "gke_password" {
-  default     = ""
-  description = "gke password"
-}
-
-variable "gke_num_nodes" {
-  default     = 2
-  description = "number of gke nodes"
-}
-
-variable "gke_cluster_name" {
-  default     = "ucluster"
-  description = "Cluster name in GKE"
-}
+variable "gke_num_nodes" { default = 2 }
+variable "gke_cluster_name" { default = "ucluster" }
